beba02735542f5a2a95772e315c9747ad056dff8
MarcoBione <marcobione.work@gmail.com>
2023-03-24 15:18:57 +0100

    Add style to app @ script + @ style

--- JS/script.js
+++ JS/script.js
@@ -139,3 +139,16 @@ const icons = [
         color: 'blue'
     }
 ];
+
+function application(){
+    
+    //load app container to script
+    const app = document.querySelector('.app');
+    //console.log('app');
+    //add style to app container
+    app.classList.add('wrapper');
+
+    
+}
+
+application();
